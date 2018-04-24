5be4469ee177f58f76d8552ad2621635d3950abd
tonaramos <gtonaramos@gmail.com>
2018-04-24 11:09:37 -0700

    Underbar Part I-completed up to _.each

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,8 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+  //[1,2,3,4,5]  n=3 
+    return n === undefined ? array[array.length - 1] : array.slice(Math.max(0, array.length - n));
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,7 +48,20 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);    
+      }
+    } else {
+      for (var element in collection) {
+        iterator(collection[element], element, collection); 
+      }
+    }
   };
+  //if the collection is array will iterate using for loop
+  //  we will pass each index of the collection into the iterator function 
+  //if the collection is object will iterate using for in method
+  //  we will pass each key of the collection into the iterator function 
 
   // Returns the index at which value can be found in the array, or -1 if value
   // is not present in the array.
